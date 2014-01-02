a9f755898fd07297031d66e93701e283ada5769a
App Academy Student <student@appacademy.io>
2014-01-02 13:35:02 -0800

    make inherits

---
+++ inherits.js
@@ -0,0 +1,34 @@
+Function.prototype.inherits = function(obj) {
+	function Surrogate() {};
+	Surrogate.prototype = obj.prototype;
+	this.prototype = new Surrogate();
+};
+
+function Animal(name) {
+	this.name = name;
+}
+
+Animal.prototype.speak = function() {
+	console.log(this.name + " says hi!");
+};
+
+function Dog(name, color) {
+	Animal.call(this, name);
+	this.color = color;
+};
+Dog.inherits(Animal);
+
+Dog.prototype.fetch = function(){
+	console.log(this.name + " fetches!");
+};
+
+dog = new Dog("Fido", "Black");
+console.log(dog.name, dog.color);
+dog.speak();
+dog.fetch();
+
+cat = new Animal("Gizmo");
+cat.speak();
+//cat.fetch();
+
+console.log(Dog.prototype instanceof Animal);
